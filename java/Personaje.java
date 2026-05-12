29134c7d435583b99f837d78ee7f1c98246cb5fa
Oscar-Berna <bernaloscarjulian@gmail.com>
2026-05-12 07:19:46 -0500

    [nivelacion-n2] bloque2 - encapsulamiento private getters setters

--- java/Personaje.java
+++ java/Personaje.java
@@ -1,34 +1,75 @@
 public class Personaje {
 
-    // Atributos del personaje
-    String nombre;
-    String clase;
-    int    nivel;
-    double vida;
-    double vidaMaxima;
-    int    ataque;
-    int    defensa;
+    private String nombre;
+    private String clase;
+    private int    nivel;
+    private double vida;
+    private double vidaMaxima;
+    private int    ataque;
+    private int    defensa;
 
-    // Constructor
+    private static final String[] CLASES = {"Guerrero", "Mago", "Arquero"};
+
     public Personaje(String nombre, String clase,
                      double vidaMax, int ataque, int defensa) {
         this.nombre     = nombre;
-        this.clase      = clase;
+        setClase(clase);
         this.nivel      = 1;
-        this.vida       = vidaMax;
         this.vidaMaxima = vidaMax;
+        this.vida       = vidaMax;
         this.ataque     = ataque;
         this.defensa    = defensa;
     }
 
-    // toString
-    @Override
-    public String toString() {
-        return String.format("[%s] %s Nv%d | HP:%.0f/%.0f",
-                clase, nombre, nivel, vida, vidaMaxima);
+    public String getNombre()     { return nombre; }
+    public String getClase()      { return clase; }
+    public int    getNivel()      { return nivel; }
+    public double getVida()       { return vida; }
+    public double getVidaMaxima() { return vidaMaxima; }
+    public int    getAtaque()     { return ataque; }
+    public int    getDefensa()    { return defensa; }
+
+    public void setVida(double v) {
+        if      (v < 0)          this.vida = 0;
+        else if (v > vidaMaxima) this.vida = vidaMaxima;
+        else                     this.vida = v;
     }
 
-    // Ejercicio 1A: barra de vida
+    public void setClase(String c) {
+        for (String valida : CLASES) {
+            if (valida.equals(c)) {
+                this.clase = c;
+                return;
+            }
+        }
+        System.out.println("Clase invalida: " + c + ". Se asigna Guerrero.");
+        this.clase = "Guerrero";
+    }
+
+    public void setNivel(int n) {
+        if      (n < 1)  this.nivel = 1;
+        else if (n > 50) this.nivel = 50;
+        else             this.nivel = n;
+    }
+
+    public void recibirDano(int dano) {
+        int danoReal = dano - this.defensa;
+        if (danoReal < 1) danoReal = 1;
+        setVida(this.vida - danoReal);
+        System.out.printf("%s recibe %d de dano. HP:%.0f%n",
+                nombre, danoReal, vida);
+    }
+
+    public void curar(double cantidad) {
+        setVida(this.vida + cantidad);
+        System.out.printf("%s curado. HP:%.0f/%.0f%n",
+                nombre, vida, vidaMaxima);
+    }
+
+    public boolean estaVivo() {
+        return this.vida > 0;
+    }
+
     public void mostrarEstado() {
         double pct     = (vida / vidaMaxima) * 100;
         int    relleno = (int)(pct / 10);
@@ -43,4 +84,10 @@ public class Personaje {
 
         System.out.printf("[%s] %.0f%% - %s%n", barra, pct, estado);
     }
-}   
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s Nv%d | HP:%.0f/%.0f",
+                clase, nombre, nivel, vida, vidaMaxima);
+    }
+}
--- java/Principal.java
+++ java/Principal.java
@@ -4,19 +4,31 @@ public class Principal {
 
         Personaje guerrero = new Personaje("Aragorn", "Guerrero", 100, 18, 12);
         Personaje mago     = new Personaje("Gandalf", "Mago",      70, 28,  5);
-        Personaje arquero  = new Personaje("Legolas", "Arquero",   85, 22,  8);
 
         System.out.println("===== PERSONAJES =====");
         System.out.println(guerrero);
         System.out.println(mago);
-        System.out.println(arquero);
 
-        guerrero.vida -= 30;
-        System.out.println("\nDespues de recibir dano:");
-        System.out.println("Vida guerrero: " + guerrero.vida);
-        System.out.println("Vida mago:     " + mago.vida);
-
-        System.out.println("\nEstado del guerrero:");
+        System.out.println("\n===== COMBATE =====");
+        guerrero.recibirDano(25);
         guerrero.mostrarEstado();
+
+        System.out.println("\n===== CURACION =====");
+        guerrero.curar(10);
+        guerrero.mostrarEstado();
+
+        System.out.println("\nGuerrero vivo: " + guerrero.estaVivo());
+
+        System.out.println("\n===== SETTER NIVEL =====");
+        guerrero.setNivel(0);
+        System.out.println("Nivel con 0:  " + guerrero.getNivel());
+        guerrero.setNivel(75);
+        System.out.println("Nivel con 75: " + guerrero.getNivel());
+        guerrero.setNivel(10);
+        System.out.println("Nivel con 10: " + guerrero.getNivel());
+
+        System.out.println("\n===== CLASE INVALIDA =====");
+        guerrero.setClase("Dios");
+        System.out.println("Clase: " + guerrero.getClase());
     }
 }
